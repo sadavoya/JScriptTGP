ac678b8bf70b9c8794898f9f57ba11c31f25bafd
the doctor <sadavoya@gmail.com>
2012-09-20 15:14:12 -0400

    added first.js

---
+++ First.js
@@ -0,0 +1,25 @@
+// First JS file - What is up doggigity
+(function myFunc() {
+    
+    var priorities = [];
+    var p = priorities;
+    p.push("Tunnel to the server");
+    p.push("Animation testing");
+    p.push("Physics");
+    p.push("Ensuring hit detection?");
+    p.push("Ok, good. Once we get that all done, we can start focussing on the rest of the reqs");
+    
+    var gripes = [];
+    var g = gripes;
+    g.push("Hitting tab to finish auto complete, bad idea.");
+    g.push("Correct. Use enter instead");
+    g.push("Autocomplete is including all of the tokens in the code, including the text!");
+    g.push("But I like enter to make a new line! I'm lazy");
+    g.push("Enter for autocomplete = minor problem. Incomplete list of autocomplete options = BIG ASS problem");
+    g.push("When changing variables it pops up autocomplete!");
+    g.push("Valid point.");
+    
+    var x = {};
+    x.foo = function() {};
+    x.foo();
+})();
